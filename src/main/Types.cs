d3394cec860da8cfd848cc3a3c4ec7c65c6c2035
elmerbool <35117496+elmerbool@users.noreply.github.com>
2026-07-26 11:49:52 +1000

    Created d23.Grannies library and updated references.

--- src/main/Types.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ei8.Cortex.Coding.d23.neurULization
-{
-    public struct Constants
-    {
-        public const int ValueToInstantiatesClassDepth = 4;
-        public const int InstanceToValueInstantiatesClassDepth = 13;
-    }
-}
--- src/main/neurULization/Types.cs
+++ src/main/neurULization/Types.cs
@@ -24,4 +24,10 @@ namespace ei8.Cortex.Coding.d23.neurULization
         Subordination = 2,
         Coordination = 4
     }
+
+    public struct Constants
+    {
+        public const int ValueToInstantiatesClassDepth = 4;
+        public const int InstanceToValueInstantiatesClassDepth = 13;
+    }
 }
